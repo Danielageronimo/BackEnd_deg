886db3744ee3194020e84d882796539d59b6e260
PORTAFOLIORC <danielageronimo087@gmail.com>
2023-03-23 15:18:06 -0300

    primera carga con backend trabajado

---
+++ src/main/java/com/portfolio/DEG/DEGApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.DEG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DEGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DEGApplication.class, args);
+	}
+
+}
